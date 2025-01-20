7cd29d0fc5986c1dc36c5751dd35ab52f928d235
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2025-01-20 15:53:29 +0700

    refactor 🔧: remove ThemeToggle component from layout for cleaner structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="fixed top-4 right-4 z-50">
-            <ThemeToggle />
-          </div>
+
           {children}
           <Toaster />
         </ThemeProvider>
